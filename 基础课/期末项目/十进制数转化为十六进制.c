31d20330410bb9f697fdbdd9c05641f83d273261
Yuzhe Jiang <55096700+ChaXerLeon-aZhe@users.noreply.github.com>
2021-03-06 21:36:02 +0800

    Delete 基础课 directory

--- 基础课/期末项目/十进制数转化为十六进制.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(int argc, char *argv[]){
-	int i;
-	printf ("please input decimalism number:\n");
-	scanf("%d",&i);
-	printf ("the hex number is %x\n",i);
-	return 0;
-}
